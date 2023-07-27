b8c45cb8ff81b7cfa59c7b84460ec88a120e5631
enzoJOtalora <126079379+enzoJOtalora@users.noreply.github.com>
2023-07-27 11:38:21 -0300

    Start work on grid generation function

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function createGrid(size){
     const gridContainer = document.getElementById('sketch-wrapper');
-    
+    for (let i = 0; i < 16; i++) {
+        const gridDiv = document.createElement('div');
+        //gridContainer.classList.remove('grid-10x10', 'grid-20x20', 'grid-30x30');
+        //gridContainer.classList.add(className);
+        gridContainer.appendChild(gridDiv);
+      }
 }
