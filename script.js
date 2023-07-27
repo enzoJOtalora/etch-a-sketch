b7750f742b97130e4c239381852f9321889e28da
enzoJOtalora <126079379+enzoJOtalora@users.noreply.github.com>
2023-07-27 20:15:22 -0300

    Create static grid

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function createGrid(size){
+function createGrid(){
     const gridContainer = document.getElementById('sketch-wrapper');
-    for (let i = 0; i < 16; i++) {
+    gridContainer.innerHTML = '';
+    for (let i = 0; i < 16*16; i++) {
         const gridDiv = document.createElement('div');
+        //gridDiv.classList.add('grid-element');
         //gridContainer.classList.remove('grid-10x10', 'grid-20x20', 'grid-30x30');
         //gridContainer.classList.add(className);
         gridContainer.appendChild(gridDiv);
       }
 }
+
+createGrid()
